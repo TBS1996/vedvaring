43d051016eb9984175218fa246a02804378d26a1
Tor Berge Saebjoernsen <tor.saebjoernsen@sentry.io>
2023-12-02 01:10:43 +0100

    wip

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use std::path::PathBuf;
+use std::fs::read_to_string;
+use serde::{Serialize, Deserialize};
+
+
+pub trait Persistence: for<'a> Deserialize<'a> + Serialize + Default {
+    fn name() -> String;
+    fn dir() -> PathBuf;
+
+    fn path() -> PathBuf {
+        let mut x = Self::dir().join(Self::name());
+        x.set_extension("toml");
+        x
+    }
+
+    fn load() -> Self {
+        let path = Self::path();
+
+        if !path.exists() {
+            Self::default().save();
+        }
+
+        let content = read_to_string(path).unwrap();
+
+        toml::from_str(content.as_str()).unwrap()
+    }
+    fn save(&self) {
+        let s = toml::to_string_pretty(self).unwrap();
+        std::fs::create_dir_all(Self::dir()).unwrap();
+        let p = Self::path();
+        std::fs::write(p.as_path(), s).unwrap();
+    }
+}
